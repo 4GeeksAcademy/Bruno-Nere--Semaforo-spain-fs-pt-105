5deff315f7495364936370dcb93bbfbe1bd5e6e8
Bruno da Silva Nere <bsnbsb@gmail.com>
2025-05-12 10:19:30 +0000

    listo

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useState } from "react"
+import ReactDOM from "react-dom/client";
+import "../styles/index.css";
 
-//Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap"
+function TrafficLight() {
+  const [color, setColor] = useState("red");
+  const changeColor = () => {
+    if (color === "red") setColor("yellow");
+    else if (color === "yellow") setColor ("green");
+    else setColor("red");
+  }
+  return (
+    <div className="traffic-light">
+      <div className={"light red" + (color === "red" ? " selected" : "")}
+      onClick={() => setColor("red")}
+      ></div>
+    <div
+    className={"light yellow" + (color ==="yellow" ? " selected" : "")}
+    onClick={() => setColor("yellow")}
+    ></div>
+    <div
+        className={"light green" + (color ==="green" ? " selected" : "")}
+    onClick={() => setColor("green")}
+    ></div>
+        <button onClick={changeColor}>Cambiar color</button>
+    </div>
+  );
+}
 
-// index.css'
-import '../styles/index.css'
-
-// components
-import Home from './components/Home';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<TrafficLight />);
